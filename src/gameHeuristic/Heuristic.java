a19d1794eee73891fa7e1fb41c49ea0cfa3360cf
pauld <pauld@PC-PDG>
2017-12-22 10:53:39 +0100

    update Heuristic

--- src/gameHeuristic/Heuristic.java
+++ src/gameHeuristic/Heuristic.java
@@ -2,12 +2,13 @@ package gameHeuristic;
 
 public class Heuristic {
 
-	int typeHeuristic;
+	int typeHeuristic; //determines which heuristic will be used.
 	
 	public Heuristic(int typeHeuristic) {
 		this.typeHeuristic = typeHeuristic;
 	}
 
+	
 	public int value(State state, Vehicle[] vehicles, int size) {
 		switch(typeHeuristic) {
 		case 2: return heuristic2(state, vehicles, size);
@@ -16,7 +17,10 @@ public class Heuristic {
 		}
 	}
 	
+	
+	
 	public int heuristic0(){
+		//constant zero
 		return 0;
 	}
 	
@@ -32,13 +36,36 @@ public class Heuristic {
 	}
 	
 	public int heuristic2(State state, Vehicle[] vehicles, int size){
-		//counting one per vehicle on the way that can move out in one move and two for vehicles on the way that can't
 		//TODO
-		int y = vehicles[0].fixedPos; //x coordinate of the red car
+		//counting one per vehicle on the way that can move out in one move and two for vehicles on the way that can't
+		int yFixed = vehicles[0].fixedPos; //x coordinate of the red car
 		int count = 0;
+		int id, y;
+		Vehicle v;
+		boolean canMoveDown,canMoveUp;
 		for (int x=state.pos[0]+vehicles[0].length; x < size ; x++) { //along the way
-			if (state.isOccupied[x][y]>0)
-				count+=1;
+			if (state.isOccupied[x][yFixed]>0) {
+				id = state.isOccupied[x][yFixed];
+				v = vehicles[id+1];
+				y = yFixed;
+				
+				canMoveDown = true;
+				//can I move down ?
+				if (v.length + yFixed + 1 < size) {
+					for (y = state.pos[id]+v.length; y < yFixed+v.length+1; y++ ) {
+						if (state.isOccupied[x][y]>0)
+							canMoveDown=false;
+					}
+				}
+				canMoveUp = true;
+				//can I move up ?
+				if (v.length <= yFixed) { //TODO
+					for (y = state.pos[id]-1; y < yFixed+v.length+1; y-- ) { //TODO
+						if (state.isOccupied[x][y]>0)
+							canMoveUp=false;
+					}
+				}
+			}
 		}
 		return count;
 		
