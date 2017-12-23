6c7233ba136431f8d5caf76080f6a70fb1fb7d4e
pauld <pauld@PC-PDG>
2017-12-23 17:11:06 +0100

    Fixed bug in merging

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 
-import GraphicGame.FileNameFilter;
 import GraphicGame.LoadingWindow;
 
 public class Main {
@@ -9,7 +8,6 @@ public class Main {
 		boolean graphicTest = false;
 		boolean useBrutForce = false;
 		int typeheuristic = 1;
-<<<<<<< HEAD
 		
 		if (graphicTest) {
 			LoadingWindow gui = new LoadingWindow(typeheuristic, useBrutForce);
@@ -20,9 +18,5 @@ public class Main {
 			CompareHeuristic c = new CompareHeuristic(0,4);
 			c.run();
 		}
-=======
-		LoadingWindow gui = new LoadingWindow(typeheuristic, useBrutForce);
-		gui.setVisible(true);
->>>>>>> branch 'master' of https://github.com/gitpdg/rushhour
 	}
 }
