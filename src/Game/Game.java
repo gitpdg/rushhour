0555ebd9becc1d265359eff8aad67c934f26d9da
Bérénice <Brnice@Haku>
2018-01-17 10:55:11 +0100

    Comment Game + seenstate. Manque solveStat a commenter

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -10,19 +10,20 @@ import java.util.Queue;
 
 public class Game {
 
-	public int size;
-	public int nbrVehicles;
-	public Vehicle[] vehicles;
-	public State initialState;
-	Heuristic heuristic;
-	boolean brutForce;
+	public int size; //Taille du plateau
+	public int nbrVehicles; //Nombre de véhicules
+	public Vehicle[] vehicles; //Tableau d'informations sur chaque véhicule, informations qui ne vont pas changer au cours de la partie
+	public State initialState; //Représente l'état initial
+	Heuristic heuristic; //Donne l'heuristique utilisée si il y a lieu
+	boolean brutForce; //Indique si on souhaite résoudre avec une heuristique ou non
 
 
 	public Game(String file_name, int typeheuristic, boolean brutForce) throws IOException {
+		//Initialise une partie et vérifie que la partie est correcte
 		BufferedReader br = new BufferedReader(new FileReader(file_name));
 		String line;
-		int size = Integer.valueOf(br.readLine());
-		int nbrVehicles = Integer.valueOf(br.readLine());
+		int size = Integer.valueOf(br.readLine()); //On récupère la taille dans le fichier
+		int nbrVehicles = Integer.valueOf(br.readLine()); //On récupère le nomre de véhicules dans le fichier
 		String[] line_split;
 		Vehicle[] pos = new Vehicle[nbrVehicles];
 		int[] posInit = new int[nbrVehicles];
@@ -32,7 +33,7 @@ public class Game {
 				isOccupied[i][j] = 0;
 		}
 		
-		for (int k = 0; k < nbrVehicles; k++) {
+		for (int k = 0; k < nbrVehicles; k++) {//Pour chaque ligne
 			line = br.readLine();
 			line_split = line.split(" ");
 			int id = Integer.valueOf(line_split[0]);
@@ -41,16 +42,17 @@ public class Game {
 			int x = Integer.valueOf(line_split[3]);
 			int y = Integer.valueOf(line_split[4]);
 			Vehicle v;
+			//On créer un nouveau véhicule et on enregistre ses informations
 			if (orientation == 'h') {
 				v = new Vehicle(id, length, orientation, y);
 				pos[k] = v;
 				posInit[k] = x;
-				if ((x-1) + (length - 1) >= size) {
+				if ((x-1) + (length - 1) >= size) { //Indique si une voiture sort du plateau ou non
 					System.out.println("Car number "+id+" is too big");
 				}
 				else {
 					for (int l = x - 1; l < x - 1 + length; l++) {
-						if (isOccupied[l][y-1] != 0)
+						if (isOccupied[l][y-1] != 0) //Indique si deux voitures sont en confrontation
 							System.out.println("Car number "+id+" and car number "+ isOccupied[l][y-1]+" overlap");
 						else
 							isOccupied[l][y-1] = v.id;
@@ -58,7 +60,7 @@ public class Game {
 				}
 			}
 
-			else {
+			else { //Idem si la voiture est verticale
 				v = new Vehicle(id, length, orientation, x);
 				pos[k] = v;
 				posInit[k] = y;
@@ -89,15 +91,17 @@ public class Game {
 
 
 	public LinkedList<Move> solve() {
+		//Résout une partie en renvoyant la liste des mouvements à effectuer depuis l'état initial
 		long startingTime = System.currentTimeMillis();
-
 		LinkedList<Move> res = new LinkedList<Move>();
-		int numberSeenStates = 0;
+		int numberSeenStates = 0; //Pour connaitre le nombre d'états visités
 		State s = this.initialState;
 		SeenStates seen = new SeenStates(this.brutForce);
-		seen.add(s, new Move(1, 0), this.nbrVehicles, this.size);
+		seen.add(s, new Move(1, 0), this.nbrVehicles, this.size); //On ajoute l'état initial comme état vu
 		boolean solved = false;
-
+		
+		//Si on utilise la méthode brute force on va utiliser un file pour faire un parcours en largeur de l'arbre des différents états possibles
+		//De cette manière si on arrive sur un état déjà vu c'est qu'on a déjà un chemin plus court menant à lui, on peut donc l'ignorer.
 		if (this.brutForce) {
 			Queue<State> q = new ArrayDeque<State>();
 			q.add(s);
@@ -105,23 +109,26 @@ public class Game {
 			while ((!q.isEmpty()) && !solved) {
 				numberSeenStates += 1;
 				s = q.remove();
+				//On teste si on est arrivé à la fin de la partie
 				int posFirstVehicle = s.pos[0];
 				int lengthFirstVehicle = (this.vehicles[0]).length;
 				if (posFirstVehicle + lengthFirstVehicle - 1 == this.size) {
 					solved = true;
 				}
 				else {
-					LinkedList<Move> moves = s.possibleMoves(this.vehicles, this.size);
+					LinkedList<Move> moves = s.possibleMoves(this.vehicles, this.size); //On regarde tous les mouvements possibles
 					for (Move m : moves){
 						State s2 = new State(s, m, this.vehicles);
-						if (seen.add(s2, m, this.nbrVehicles, this.size)[0] == -1){
-							q.add(s2);
+						if (seen.add(s2, m, this.nbrVehicles, this.size)[0] == -1){ //Si l'état s2 n'est pas déjà vu on le marque comme vu (ce qui se fait dans add)
+							q.add(s2); //Puis on l'ajoute à la file des états à visiter
 						}
 					}
 				}
 			}
 		}
-
+		
+		
+		//Si on souhaite utiliser une heuristique, on va alors utiliser une file de priorité afin de prendre les états par distance + heuristique croissante
 		else {
 			PriorityQueue<State> q = new PriorityQueue<State>(nbrVehicles, new ComparatorState());
 			s.setheuristic(heuristic, vehicles, size);
@@ -130,9 +137,10 @@ public class Game {
 			
 			while ((!q.isEmpty()) && !solved) {
 				numberSeenStates += 1;
-				s = q.poll();
-				if (seen.isExplored(s, this.nbrVehicles, this.size) == 0){
-					seen.explore(s, this.nbrVehicles, this.size);
+				s = q.poll(); //On récupère l'état le plus proche au sens distance + heuristique. Sa distance actuelle à l'état initial est alors sa distance minimale (cf preuve correction).
+				if (seen.isExplored(s, this.nbrVehicles, this.size) == 0){ //Si l'état a déjà été exploré c'est qu'on a déjà trouvé sa distance minimale a l'état initial. On ne regarde donc que des états non explorés.
+					seen.explore(s, this.nbrVehicles, this.size); //On marque cet état comme exploré puisqu'on a trouvé sa distance à l'état initial.
+					//On teste si on est arrivé à la fin de la partie
 					int posFirstVehicle = s.pos[0];
 					int lengthFirstVehicle = (this.vehicles[0]).length;
 					if (posFirstVehicle + lengthFirstVehicle - 1 == this.size) {
@@ -140,21 +148,21 @@ public class Game {
 					}
 					else {
 						LinkedList<Move> moves = s.possibleMoves(this.vehicles, this.size);
-						for (Move m : moves){
+						for (Move m : moves){ //Pour chaque mouvement possible
 							State s2 = new State(s, m, this.vehicles);
-							s2.setdistance(s.distance + 1);
+							s2.setdistance(s.distance + 1); //On connait un chemin de longueur s.distance + 1 à l'état initial.
 							int[] vu = seen.add(s2, m, this.nbrVehicles, this.size);
 							
-							if (vu[0] == -1){
-								s2.setheuristic(heuristic, vehicles, size);
-								seen.changedHeuristic(s2, this.nbrVehicles, this.size);
-								q.add(s2);
+							if (vu[0] == -1){ //Si l'état n'avait pas du tout été déjà vu
+								s2.setheuristic(heuristic, vehicles, size); //On calcule sont heuristique
+								seen.changedHeuristic(s2, this.nbrVehicles, this.size); //Qu'on enregistre dans seen
+								q.add(s2); //On ajoute s2 à la liste des états à explorer
 							}
-							else{
-								if (vu[1] == 0){
-									if (s.distance + 1 < vu[0]){
+							else{ //Si on avait déjà vu cet état
+								if (vu[1] == 0){ //Si il n'est pas déjà exploré
+									if (s.distance + 1 < vu[0]){ //Et si ce nouveau chemin est plus court que l'ancien qu'on avait trouvé
 										s2.heuristic = vu[2];
-										q.add(s2);
+										q.add(s2); //On ajouter s2 à la liste des états à explorer. On n'a pas besoin de supprimer l'ancien état qu'on avait ajouté car le nouvel état sera dans tous les cas traité avant.
 									}
 								}
 							}
@@ -167,7 +175,7 @@ public class Game {
 
 		LinkedList<Move> resInv = new LinkedList<Move>();
 
-		if (solved) {
+		if (solved) { //Si on a réussi à résoudre la partie, on remonte en partant de l'état final pour obtenir la liste des mouvements à faire
 			Move lastmove = s.getLastMove(seen, this.nbrVehicles, this.size);
 			s.Previous(lastmove);
 			res.add(lastmove);
@@ -179,7 +187,7 @@ public class Game {
 
 
 			Iterator<Move> ite = res.descendingIterator();
-			while (ite.hasNext()) {
+			while (ite.hasNext()) { //On inverse ensuite la liste pour l'avoir dans le bon sens.
 				resInv.add(ite.next());
 			}
 			System.out.println("Execution time : "+ (System.currentTimeMillis()-startingTime)+ "ms");
--- src/Game/SeenStates.java
+++ src/Game/SeenStates.java
@@ -20,8 +20,13 @@ public class SeenStates {
 	}
 	
 	public int[] add(State e, Move lastmove, int nbrVehicles, int size){
-		//Indique si un état a déjà été vu ou non, et si ce n'est pas le cas le marque comme vu et enregistre
-		//le dernier mouvement fait pour arriver dans cet état.
+		//Indique si un état a déjà été vu ou non, et renvoie si c'est le cas des informations sur cet état :
+		//	- Sa distance à l'état initial actuellement connue
+		//	- Est ce que l'état est déjà exploré ou non
+		//	- La valeur de l'heuristic en cet état
+		//Si l'état n'a pas déjà été vu on enregistre ces différentes informations ainsi que le dernier mouvement fait pour arriver dans cet état
+		//Enfin si l'état a déjà été vu mais avec une distance plus grande on met alors à jour la distance et le dernier mouvement fait.
+		//Dans le cas où on n'utilise pas les heuristics on s'intéresse uniquement à si l'état à déjà été vu ou non.
 		int[] pos = e.pos;
 		SeenStates tree = this;
 		int[] res = new int[3];
@@ -37,31 +42,31 @@ public class SeenStates {
 			tree = child[pos[i]-1];			
 		}
 		if (tree.lastmove == null) { //Si on a pas encore vu cet état
-			tree.lastmove = lastmove;
+			tree.lastmove = lastmove; //On enregistre le dernier mouvement fait
 			if (!this.brutForce){
-				tree.distance = e.distance;
+				tree.distance = e.distance; //On enregistre la distance actuelle et l'heuristique de cet état
 				tree.heuristic = e.heuristic;
-				res[0] = -1;
-				res[1] = 0;
-				res[2] = -1;
+				res[0] = -1; //On indique qu'on n'avait pas encore vu cet état
+				res[1] = 0;	//Qu'il n'avait pas non plus été déjà exploré
+				res[2] = -1; //Et qu'on avait pas encore calculé l'heuristiqu
 			}
 			else{
-				res[0] = -1;
+				res[0] = -1; //Dans le cas bruteForce on indique juste qu'on avait pas encore vu l'état
 			}
 		}
-		else {
+		else {	//Si on a déjà vu l'état
 			
 			if (!this.brutForce){
-				res[0] = tree.distance;
-				res[1] = tree.isExplored;
-				res[2] = tree.heuristic;
-				if (tree.distance > e.distance){
-					tree.distance = e.distance;
-					tree.lastmove = lastmove;
+				res[0] = tree.distance; //On renvoie la distance actuelle à l'état initial
+				res[1] = tree.isExplored; //On indique si l'état a déjà été exploré ou non
+				res[2] = tree.heuristic; //On renvoie l'heuristique pour ne pas avoir à la calculer deux fois
+				if (tree.distance > e.distance){ //Si on a maintenant un chemin plus court pour y arriver
+					tree.distance = e.distance; //On met à jour la distance
+					tree.lastmove = lastmove; //On met à jour le dernier mouvement
 				}
 			}
 			else{
-				res[0] = 0;
+				res[0] = 0; //Dans le cas bruteForce on indique juste qu'on a déjà vu l'état
 			}
 		}
 		return res;
